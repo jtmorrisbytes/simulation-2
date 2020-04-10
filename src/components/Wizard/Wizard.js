f5cef986d2ad44190e1290f77f543aa3c8fc438f
= <=>
2020-04-10 16:27:25 -0500

    finished input handling on wizard

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -3,7 +3,18 @@ import "./Wizard.scss";
 class Wizard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(e) {
+    console.log(e.target.name);
+    this.setState({ [e.target.name]: e.target.value });
   }
   render() {
     return (
@@ -15,24 +26,49 @@ class Wizard extends Component {
         <div className="container">
           <div className="name">
             <span>Property Name</span> <br />
-            <input name="name" type="text" />
+            <input
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              name="name"
+              type="text"
+            />
           </div>
           <div className="address">
             <span>Address</span> <br />
-            <input name="address" type="text" />
+            <input
+              value={this.state.address}
+              onChange={this.handleInputChange}
+              name="address"
+              type="text"
+            />
           </div>
           <div className="inline">
             <div className="city">
               <span>City</span> <br />
-              <input name="city" type="text" />
+              <input
+                value={this.state.city}
+                onChange={this.handleInputChange}
+                name="city"
+                type="text"
+              />
             </div>
             <div className="state">
               <span>State</span> <br />
-              <input name="state" type="text" />
+              <input
+                value={this.state.state}
+                name="state"
+                onChange={this.handleInputChange}
+                type="text"
+              />
             </div>
             <div className="zip">
               <span>Zip</span> <br />
-              <input name="zip" type="number" />
+              <input
+                name="zip"
+                value={this.state.zip}
+                onChange={this.handleInputChange}
+                type="tex "
+              />
             </div>
           </div>
         </div>
