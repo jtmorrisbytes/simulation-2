9e292d1e55246d14f533738451a9a59923e78f83
= <=>
2020-04-10 16:15:00 -0500

    finish basic styling

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
-
+import "./Wizard.scss";
 class Wizard extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div className="Wizard">hello from Wizard</div>;
+    return (
+      <main className="Wizard">
+        <div className="header-container">
+          <h3>Add New listing</h3>
+          <button className="cancel">cancel</button>
+        </div>
+        <div className="container">
+          <div className="name">
+            <span>Property Name</span> <br />
+            <input name="name" type="text" />
+          </div>
+          <div className="address">
+            <span>Address</span> <br />
+            <input name="address" type="text" />
+          </div>
+          <div className="inline">
+            <div className="city">
+              <span>City</span> <br />
+              <input name="city" type="text" />
+            </div>
+            <div className="state">
+              <span>State</span> <br />
+              <input name="state" type="text" />
+            </div>
+            <div className="zip">
+              <span>Zip</span> <br />
+              <input name="zip" type="number" />
+            </div>
+          </div>
+        </div>
+        <button className="continue">Continue</button>
+      </main>
+    );
   }
 }
 export default Wizard;
