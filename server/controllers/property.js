946855fb228ecf5bdbb0de4e9daab52dae31febd
= <=>
2020-04-11 12:12:38 -0500

    commit

--- server/controllers/property.js
+++ server/controllers/property.js
@@ -1,5 +1,12 @@
 module.exports = {
-  getAll: function (req, res) {
-    res.sendStatus(200);
+  getAll: async function (req, res) {
+    const db = req.app.get("db");
+    try {
+      let result = await db.property.getAll();
+      res.json(result);
+    } catch (e) {
+      console.log(e);
+      res.status(500).json(e);
+    }
   },
 };
